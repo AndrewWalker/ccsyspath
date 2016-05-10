81a3333d1a6445b712dca949bc3a1e954d0a9c56
Andrew Walker <walker.ab@gmail.com>
2016-05-10 10:11:16 +1000

    attempted fix for windows

--- ccsyspath/paths.py
+++ ccsyspath/paths.py
@@ -1,21 +1,34 @@
 import subprocess
 from subprocess import PIPE
 import os
+import re
 
-def system_include_paths(compiler, cpp=True):
+def compiler_preprocessor_verbose(compiler, extraflags):
+    """Capture the compiler preprocessor stage in verbose mode
+    """
     lines = []
     with open(os.devnull, 'r') as devnull:
         cmd = [compiler, '-E'] 
-        if cpp:
-            cmd += ['-x', 'c++']
+        cmd += extraflags
         cmd += ['-', '-v']
         p = subprocess.Popen(cmd, stdin=devnull, stdout=PIPE, stderr=PIPE)
         p.wait()
         lines = p.stderr.read()
         lines = lines.splitlines()
+    return lines
+
+
+def system_include_paths(compiler, cpp=True):
+    extraflags = []
+    if cpp:
+        extraflags = b'-x c++'.split()
+    lines = compiler_preprocessor_verbose(compiler, extraflags)
 
     idx = lines.index(b'#include <...> search starts here:')
     lines = lines[idx+1:-2]
-    lines = [ line[1:] for line in lines ]
-    paths = [ line.split(b' ')[0] for line in lines]
+    paths = []
+    for line in lines:
+        line = line.replace(b'(framework directory', b'')
+        line = line.strip()
+        paths.append(line)
     return paths 
